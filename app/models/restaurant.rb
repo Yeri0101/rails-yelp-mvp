b73c29c546bb92a38afa5abdf69ddd7450e4f9a8
yerinto martinez <yerinto.martinez@hotmail.fr>
2022-11-16 18:10:14 +0100

    rails-yealp-mvp_version_2

--- app/models/restaurant.rb
+++
@@ -1,6 +0,0 @@
-class Restaurant < ApplicationRecord
-  has_many :reviews, dependent: :destroy
-  Categories = ["chinese", "italian", "japanese", "french", "belgian"]
-  validates :name, :address, presence: true
-  validates :category, inclusion: { in: Categories }
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create!(
+  name: "	Raven Belge",
+  address: "Rue du Bon-Secours, Brussel",
+  phone_number: "+253 23 24 67 83",
+  category: "belgian"
+)
+
+Restaurant.create!(
+  name: "Epicure",
+  address: "Faubourg Saint Honoré, Paris",
+  phone_number: "+333 25  52 98",
+  category: "french"
+)
+
+Restaurant.create!(
+  name: "Bueno",
+  address: "Strausberger Platz, Berlin",
+  phone_number: "+451 23 24 67 83",
+  category: "italian"
+)
+
+Restaurant.create!(
+  name: "The Best one",
+  address: "Ferdinand Bolstraat, Amsterdam",
+  phone_number: "+211 23 24 67 83",
+  category: "japanese"
+)
+
+Restaurant.create!(
+  name: "Chino",
+  address: "Shoreditch, London",
+  phone_number: "+424 453 45 67 89",
+  category: "chinese"
+)
